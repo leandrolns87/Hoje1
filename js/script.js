6dc01d64e3054b3afb84e0a5dc1456985950f545
Claudiapersa <92328069+Persa1109@users.noreply.github.com>
2025-09-22 21:31:54 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Conversor de Temperatura</title>
+
+<link rel="stylesheet" href="css/style.css">
+
+<div class="container">
+
+    <h1>Conversor Celsius para Fahrenheit</h1>
+
+    <input type="number" id="celsius" placeholder="Digite a temperatura em Celsius">
+
+
+    <button onclick="converter()">Converter</button>
+
+
+    <h2 id="resultado">O resultado aparecerá aqui</h2>
+
+
+</div> <script src="js/script.js"></script>
